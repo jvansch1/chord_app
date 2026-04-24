c6928ec7fa244411c772ecefc228885a934345a4
John <jvansch1@binghamton.edu>
2026-04-24 06:47:56 -0400

    Add fastapi.

--- src/main.py
+++ src/main.py
@@ -1,22 +1,17 @@
-from minor_chord import MinorChord
-from constants import FLAT
-from constants import MINOR
 from chord_factory import chord_factory
 from note import Note
 
 from answer_parser import parse_answer
 from chord import Chord
 
+from smtplib import SMTP
+
     
         
 def check_answer(chord: Chord, notes: list[Note]):
     expected_note_names = chord.get_note_names()
 
-    print(f"notes: {[str(note) for note in notes]}")
-    print(f"expected_note_names: {[str(note) for note in expected_note_names]}")
-
     for note in expected_note_names:
-        print(note)
         if note not in notes:
             print("Not Correct")
             return
@@ -25,10 +20,8 @@ def check_answer(chord: Chord, notes: list[Note]):
 
 
 if __name__ == "__main__":
-    # random_note = Note.random_note()
-    random_note = Note('G', FLAT)
-    # chord = chord_factory(random_note)
-    chord = MinorChord(random_note, MINOR)
+    random_note = Note.random_note()
+    chord = chord_factory(random_note)
     answer = input(f"{str(chord)}\n")
     individual_note_names = parse_answer(answer)
 
