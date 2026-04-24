6251c50daeaf006cb854a6f21576046d2fb3a8b3
John <jvansch1@binghamton.edu>
2026-04-24 06:53:53 -0400

    Add chord endpoint.

--- src/api/chord.py
+++ src/api/chord.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 
+from chord_factory import chord_factory
+from note import Note
+
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def get_chord():
+    random_note = Note.random_note()
+    chord = chord_factory(random_note)
+    return {
+        "chord": str(chord),
+        "chord_notes": chord.get_note_names()
+    }
